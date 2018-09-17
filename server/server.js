00aefc4d7c7b949634437366c7a2ac07d2933745
Rasheed M <r.muslimani@gmail.com>
2018-09-17 17:57:44 -0400

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -98,7 +98,7 @@ app.patch('/todos/:id', (req, res) => {
 		body.completedAt = new Date().getTime();
 	} else {
 		body.completed = false;
-		body.completeAt = null;
+		body.completedAt = null;
 	}
 	
 	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
