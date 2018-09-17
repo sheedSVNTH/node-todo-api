b086e8ab426d247003a69ce3434983bc1a75b75c
Rasheed M <r.muslimani@gmail.com>
2018-09-17 14:36:23 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,37 @@
 //256 # of bits
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+//bcrypt.genSalt(10, (err, salt) => {
+//	bcrypt.hash(password, salt, (err, hash) => {
+//		console.log(hash);
+//	});
+//});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$1c1g/DxgR9rXkJrEYaKMDOlYN/ClM5WQ6cWsUe0uQVfsC8XL7upTW';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+//Gen Salt
+//Hash Password
+
+
+
+
+//var data = {
+//	id: 10
+//};
+//
+//var token = jwt.sign(data, '123abc');
+//console.log(token);
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log('decoded', decoded);
 
 
 //var message = 'I am user number 3 crypto dog.';
